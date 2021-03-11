b1d625ec3166c18d4a16592cdd15c4a4ec124fed
swp <songwenping@shzhanmeng.com>
2021-03-11 16:54:50 +0800

    mod

--- clone.js
+++ clone.js
@@ -7,42 +7,46 @@ let ProgressBar = require('progress');
 
 var WorkSpacePath = "F:\\code_store"
 
-var url = "https://gitcode.wonderful-app.com/api/v4/projects?private_token=cNDPKW76XEAs2uxC8VjD";
+var url = "https://gitcode.wonderful-app.com/api/v4/projects?private_token=cNDPKW76XEAs2uxC8VjD&per_page=100";
 var repos = []; //仓库列表
 
 /**获取仓库列表 */
-function reqHttp() {
-    return new Promise(function(resolve, reject) {
-        https.get(url, function(res) {
+function reqHttp(page) {
+    return new Promise(function (resolve, reject) {
+        https.get(url + `&page=${page}`, function (res) {
             let bufs = "";
-            res.on("data", function(data) {
+            res.on("data", function (data) {
                 bufs += data;
             });
-            res.on("end", function() {
-                repos = JSON.parse(bufs);
+            res.on("end", function () {
+                repos = repos.concat(JSON.parse(bufs));
                 resolve()
             })
-        }).on("error", function() {
-            reject();
+        }).on("error", function (error) {
+            console.error(error)
+            reject(error);
         })
     })
 }
 
 /**克隆仓库 */
 function cloneRepo() {
-    reqHttp().then(function() {
+    Promise.all([
+        reqHttp(1),
+        reqHttp(2)
+    ]).then(function () {
         if (!repos || !repos.length) return;
-
         let total = repos.length;
         let progressBar = new ProgressBar(':bar :percent', {
             complete: '█',
             total: total,
             width: 60
         })
-
+        console.log(`开始克隆 ${total}个项目`);
+        let num = 0;
         doTaskList(() => {
             if (!repos.length) {
-                console.log(`克隆完成 当前克隆了${total}个项目`)
+                console.log(`克隆完成 当前克隆了${num}个项目`)
                 return null
             };
             let data = repos.shift();
@@ -58,11 +62,12 @@ function cloneRepo() {
                 let cmd = `git clone ${ssh_url_to_repo}`;
                 execSync(cmd, { cwd: repoDir, encoding: 'utf8' });
                 progressBar.tick();
-                return Promise.resolve();
+                num++;
             } else {
-                fs.removeSync(repoPath);
-                return null;
+                console.log(`目录${namespaceName}/${repoName}已存在`)
             }
+
+            return Promise.resolve();
         })
     })
 }
